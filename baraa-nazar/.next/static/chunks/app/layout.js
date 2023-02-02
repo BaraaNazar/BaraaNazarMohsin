aedbdf50fee5adacb740a82d22ef7e11fddc8d51
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-02-02 11:58:32 +0300

    Main

---
+++ baraa-nazar/.next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fbaraanazar%2FBaraaNazarMohsin%2Fbaraa-nazar%2Fsrc%2Fapp%2Fglobals.css&server=false!":
+/*!************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fbaraanazar%2FBaraaNazarMohsin%2Fbaraa-nazar%2Fsrc%2Fapp%2Fglobals.css&server=false! ***!
+  \************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"_app-client_src_app_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/globals.css */ \"(app-client)/./src/app/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRlVzZXJzJTJGYmFyYWFuYXphciUyRkJhcmFhTmF6YXJNb2hzaW4lMkZiYXJhYS1uYXphciUyRnNyYyUyRmFwcCUyRmdsb2JhbHMuY3NzJnNlcnZlcj1mYWxzZSEuanMiLCJtYXBwaW5ncyI6IkFBQUEsT0FBTywrTEFBc0ciLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz8wMWJjIl0sInNvdXJjZXNDb250ZW50IjpbIigoKSA9PiBpbXBvcnQoLyogd2VicGFja01vZGU6IFwibGF6eVwiICovIFwiL1VzZXJzL2JhcmFhbmF6YXIvQmFyYWFOYXphck1vaHNpbi9iYXJhYS1uYXphci9zcmMvYXBwL2dsb2JhbHMuY3NzXCIpKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fbaraanazar%2FBaraaNazarMohsin%2Fbaraa-nazar%2Fsrc%2Fapp%2Fglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fbaraanazar%2FBaraaNazarMohsin%2Fbaraa-nazar%2Fsrc%2Fapp%2Fglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
